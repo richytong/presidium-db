a8e353929f1f5cd6f846c187ac8e7d4f840f8ac9
Richard <richytong@gmail.com>
2026-04-30 14:43:46 -0700

    benchmarks

--- DiskSortedHashTable.benchmarks.js
+++ DiskSortedHashTable.benchmarks.js
@@ -5,8 +5,8 @@ async function runBenchmark(degree, size) {
   const ht = new DiskSortedHashTable({
     storagePath: `${__dirname}/DiskSortedHashTable_test_data/${size}`,
     headerPath: `${__dirname}/DiskSortedHashTable_test_data/${size}_header`,
-    initialLength: size * 8,
-    itemSize: 2048 * 10,
+    initialLength: size * 2,
+    itemSize: 100,
     sortValueType: 'number',
     resizeRatio: 0,
     degree,
@@ -15,7 +15,7 @@ async function runBenchmark(degree, size) {
   await ht.init()
 
   const insertedNumbers = []
-  for (const n of randomUniqueNumbersGenerator(size, size * 1024)) {
+  for (const n of randomUniqueNumbersGenerator(size, size * 2)) {
     const start = performance.now()
     await ht.set(`key${n}`, `value${n}`, n)
     console.log('set', `key${n}`, `value${n}`, n, 'in', `${performance.now() - start}ms, degree ${degree}, current count ${ht.count()}`)
@@ -29,4 +29,4 @@ async function runBenchmark(degree, size) {
   }
 }
 
-runBenchmark(2, 1024 * 1024)
+runBenchmark(2, 1_000_000_000)
