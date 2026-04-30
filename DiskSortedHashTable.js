588d3dab02349aa4aebd788a5e2bee894e26ab5c
Richard <richytong@gmail.com>
2026-04-30 15:42:52 -0700

    docs

--- DiskSortedHashTable.js
+++ DiskSortedHashTable.js
@@ -41,7 +41,7 @@ const REMOVED = 2
  *     * `storagePath` - `string` - the path to the file used to store the disk sorted hash table data.
  *     * `headerPath` - `string` - the path to the file used to store header information about the disk sorted hash table.
  *     * `initialLength` - `number` - the initial length of the disk sorted hash table. Minimum value 1024, maximum value 2147483647. Defaults to 1024.
- *     * `itemSize` - `number` - the size in bytes of each item stored on disk. Minimum value 1024. Defaults to 524288.
+ *     * `itemSize` - `number` - the size in bytes of each item (including internal item info, key, value, and sortValue) stored on disk. Minimum value 1024. Defaults to 524288.
  *     * `sortValueType` - `'string'|'number'` - the type of the disk sorted hash table sort-values.
  *     * `resizeRatio` - `number` - the ratio of number of items to table length at which to resize the disk sorted hash table. Minimum value 0 (no resize), maximum value 1. Defaults to 0.
  *     * `resizeFactor` - `number` - the factor that is multiplied with the disk sorted hash table's current length to determine the new table length on a resize.
