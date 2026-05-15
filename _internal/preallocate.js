990faf548be0296986787dd7e64df1c93c7a0fc1
Richard <richytong@gmail.com>
2026-05-15 11:44:47 -0700

    preallocate for macos

--- _internal/preallocate.js
+++ _internal/preallocate.js
@@ -1,12 +1,20 @@
 const { execSync } = require('child_process')
 const fs = require('fs')
+const os = require('os')
 
 /**
  * Pre-allocates a file using the 'fallocate' system utility.
  * This is extremely fast and ensures contiguous disk blocks.
  */
 function preallocate(filePath, sizeInBytes) {
-  execSync(`fallocate -l ${sizeInBytes} ${filePath}`)
+  const platform = os.platform()
+  if (platform == 'linux') {
+    execSync(`fallocate -l ${sizeInBytes} ${filePath}`)
+  } else if (platform == 'darwin') {
+    execSync(`mkfile ${sizeInBytes} ${filePath}`)
+  } else {
+    throw new Error(`unsupported platform ${platform}`)
+  }
 }
 
 module.exports = preallocate
---
+++ _internal/preallocate.test.js
@@ -0,0 +1,45 @@
+const Test = require('thunk-test')
+const assert = require('assert')
+const fs = require('fs')
+const os = require('os')
+const preallocate = require('./preallocate')
+
+const test = new Test('preallocate', async function integration() {
+
+  {
+    const filepath = `${__dirname}/preallocate_test`
+    await fs.promises.rm(filepath).catch(() => {})
+
+    await preallocate(filepath, 100)
+
+    let stats = await fs.promises.stat(filepath)
+    assert.strictEqual(stats.size, 100)
+
+    await preallocate(filepath, 200)
+
+    stats = await fs.promises.stat(filepath)
+    assert.strictEqual(stats.size, 200)
+
+    await fs.promises.rm(filepath)
+  }
+
+  const osPlatform = os.platform
+  os.platform = () => 'unknown'
+
+  {
+    const filepath = `${__dirname}/preallocate_test_unknown`
+    await assert.throws(
+      () => preallocate(filepath, 100),
+      new Error('unsupported platform unknown')
+    )
+  }
+
+  os.platform = osPlatform
+
+}).case()
+
+if (process.argv[1] == __filename) {
+  test()
+}
+
+module.exports = test
